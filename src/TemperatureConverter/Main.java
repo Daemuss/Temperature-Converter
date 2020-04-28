063e226c6c586b3757c1b685b887fba1e31e0d6e
Daemuss <alfiemondejar1@outlook.com>
2020-04-28 15:14:13 +0200

    Initial files

---
+++ src/TemperatureConverter/Main.java
@@ -0,0 +1,23 @@
+package TemperatureConverter;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = new GridPane();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
